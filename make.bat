9af23b0e074e9eed22d9dff2e1f3d815d38857f3
toxa <untoxa@mail.ru>
2020-04-26 00:22:56 +0300

    sources moved to src

--- make.bat
+++ make.bat
@@ -2,7 +2,8 @@
 @set PROJ=sdcc4farcalls
 @set GBDK=..\..\gbdk
 @set GBDKLIB=%GBDK%\lib\small\asxxxx
-@set OBJ=build
+@set OBJ=build\
+@set SRC=src\
 
 @set CFLAGS=-mgbz80 --no-std-crt0 -Dnonbanked= -I %GBDK%\include -I %GBDK%\include\asm -I src\include -c
 @rem set CFLAGS=%CFLAGS% -DUSE_SFR_FOR_REG
@@ -23,22 +24,22 @@
 
 @echo ASSEMBLING THE STUB...
 
-sdasgb %ASMFLAGS% %OBJ%\MBC1_RAM_INIT.rel MBC1_RAM_INIT.s
-sdasgb %ASMFLAGS% %OBJ%\__sdcc_call_hl.rel __sdcc_call_hl.s
-@set LFILES=%LFILES% %OBJ%\MBC1_RAM_INIT.rel %OBJ%\__sdcc_call_hl.rel
+sdasgb %ASMFLAGS% %OBJ%MBC1_RAM_INIT.rel %SRC%MBC1_RAM_INIT.s
+sdasgb %ASMFLAGS% %OBJ%__sdcc_call_hl.rel %SRC%__sdcc_call_hl.s
+@set LFILES=%LFILES% %OBJ%MBC1_RAM_INIT.rel %OBJ%__sdcc_call_hl.rel
 
 @echo COMPILING WITH SDCC4...
 
-sdcc %CFLAGS% sdcc4bank1code.c -bo1 -o %OBJ%\sdcc4bank1code.rel
-sdcc %CFLAGS% sdcc4bank2code.c -bo2 -o %OBJ%\sdcc4bank2code.rel
-@set LFILES=%LFILES% %OBJ%\sdcc4bank1code.rel %OBJ%\sdcc4bank2code.rel 
+sdcc %CFLAGS% %SRC%sdcc4bank1code.c -bo1 -o %OBJ%sdcc4bank1code.rel
+sdcc %CFLAGS% %SRC%sdcc4bank2code.c -bo2 -o %OBJ%sdcc4bank2code.rel
+@set LFILES=%LFILES% %OBJ%sdcc4bank1code.rel %OBJ%sdcc4bank2code.rel 
 
-sdcc %CFLAGS% %PROJ%.c -o %OBJ%\%PROJ%.rel
+sdcc %CFLAGS% %SRC%%PROJ%.c -o %OBJ%%PROJ%.rel
 
 @echo PATCHING...
-@python far_fixer.py %LFILES% %OBJ%\%PROJ%.rel > %OBJ%\%PROJ%.fixed.rel
+@python far_fixer.py %LFILES% %OBJ%%PROJ%.rel > %OBJ%%PROJ%.fixed.rel
 
 @echo LINKING WITH GBDK...
-%GBDK%\bin\link-gbz80 %LFLAGS% %PROJ%.gb %LFILES% %OBJ%\%PROJ%.fixed.rel 
+%GBDK%\bin\link-gbz80 %LFLAGS% %PROJ%.gb %LFILES% %OBJ%%PROJ%.fixed.rel 
 
 @echo DONE!
